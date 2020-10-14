de305902c66ec18e2a90e39acb178052c382c02b
dayam <dayam@Xs-MacBook-Air.local>
2020-10-14 15:16:59 -0600

    Changed to output RData file as required by GENESIS SevenBridges pipeline

--- run_get_valid_snp_ids.R
+++ run_get_valid_snp_ids.R
@@ -6,8 +6,7 @@ out.file.name=args[3]
 info <- read.delim(gzfile(info.gz.file), stringsAsFactors=F)
 info$Rsq_num <- as.numeric(info$Rsq)
 extracted.rows <- (info$Genotyped == "Typed_Only") | (!is.na(info$Rsq_num) & (info$Rsq_num >= min.rsq))  
-snps <- info$SNP[extracted.rows]
+variant.id <- data.frame(variant.id=info$SNP[extracted.rows])
 
-write.table(snps, out.file.name,  sep="\t", quote=F, row.names=F, col.names=F)
-
+save(variant.id, file=out.file.name)
 
