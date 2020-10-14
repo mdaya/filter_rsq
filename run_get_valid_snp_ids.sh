41a6303ec3ec62204a59f59edc84466c15491a38
dayam <dayam@Xs-MacBook-Air.local>
2020-10-14 15:16:57 -0600

    Changed to output RData file as required by GENESIS SevenBridges pipeline

--- run_get_valid_snp_ids.sh
+++ run_get_valid_snp_ids.sh
@@ -2,6 +2,6 @@
 
 info_gz_file=$1
 min_rsq=$2
-out_file_name=`basename $info_gz_file | sed 's/.info.gz/_valid_snp_IDs.txt/'`
+out_file_name=`basename $info_gz_file | sed 's/.info.gz/_valid_snp_IDs.RData/'`
 
 cat /home/analyst/run_get_valid_snp_ids.R | R --vanilla --args $info_gz_file $min_rsq $out_file_name
